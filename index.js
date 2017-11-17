0d65280489e3b92ac294eade37edf68deae47ae3
Dean Stigall <stigallrealestate@gmail.com>
2017-11-17 19:48:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 // Write your solution in this file!
 customerName = 'bob';
-setBestCustomer = '';
+
 function upperCaseCustomerName(){
   customerName= customerName.toUpperCase();
 }
+function setBestCustomer({
+  
+})
